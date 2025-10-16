94f1a0fe84bc1526aaee4d0cc6f51cb69b496d0a
rkgmf1230 <rkgmf1230@nexlink.co.kr>
2025-10-16 13:54:54 +0900

    배포테스트4

--- kt-vite/src/main.jsx
+++ kt-vite/src/main.jsx
@@ -6,6 +6,9 @@ import { router } from "./router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} basename="/react/kt-vite" />
+    <RouterProvider
+      router={router}
+      basename={import.meta.env.VITE_PUBLIC_URL}
+    />
   </StrictMode>
 );

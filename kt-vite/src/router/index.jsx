e4e1fa721e051234aab73da35b83beb40b989bfe
rkgmf1230 <rkgmf1230@nexlink.co.kr>
2025-10-16 13:44:17 +0900

    배포테스트

--- kt-vite/src/router/index.jsx
+++ kt-vite/src/router/index.jsx
@@ -3,23 +3,28 @@ import MainPage from "../pages/MainPage";
 import MonitoringPage from "../pages/MonitoringPage";
 import SettingsPage from "../pages/SettingsPage";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <MainPage />,
+      children: [
+        {
+          index: true,
+          element: <Navigate to="/monitoring" replace />,
+        },
+        {
+          path: "monitoring",
+          element: <MonitoringPage />,
+        },
+        {
+          path: "settings",
+          element: <SettingsPage />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <MainPage />,
-    children: [
-      {
-        index: true,
-        element: <Navigate to="/monitoring" replace />,
-      },
-      {
-        path: "monitoring",
-        element: <MonitoringPage />,
-      },
-      {
-        path: "settings",
-        element: <SettingsPage />,
-      },
-    ],
-  },
-]);
+    basename: "/react/kt-vite",
+  }
+);
